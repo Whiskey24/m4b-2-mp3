f241a644046745d8c0651b3c5cff050900abb00a
Whiskey24 <bert@santema.eu>
2023-09-15 00:06:21 +0200

    Sanitize filenames and other bits
    - check if FFMPEG can be found
    - store file with chapter timings inside book directory so multiple sessions can be run at the same time
    - sanitize filenames for Windows
    - fixed bug for chapter names with special chars

--- convert.sh
+++ convert.sh
@@ -16,6 +16,12 @@ CYAL='\033[1;36m'
 
 NCLR='\033[0m' # No Color
 
+## Test if FFMPEG can be Found
+if [ ! -f "$FFMPEG" ]; then
+    echo "Cannot find ffmpeg at $FFMPEG"
+	exit
+fi
+
 # create MP3DIR if it does not exist
 mkdir -p $MP3DIR
 
@@ -27,6 +33,19 @@ readarray -d '' BOOKLIST < <(find $M4BDIR -type f -iname "*$SEARCHTERM*.m4b" -pr
 
 
 LENGTH=${#BOOKLIST[@]}
+
+# Block until the given file appears or the given timeout is reached.
+# Exit status is 0 if the file exists.
+# https://superuser.com/questions/878640/unix-script-wait-until-a-file-exists
+waitFile() {
+    local file="$1"; shift
+    local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+    test $wait_seconds -lt 1 && echo 'At least 1 second is required' && return 1
+
+    until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
+
+    test $wait_seconds -ge 0 # equivalent: let ++wait_seconds
+}
 
 # function to show list with options to user
 showBookList(){
@@ -62,6 +81,7 @@ enterTitle(){
 convertToMp3(){
         OUTDIR="$MP3DIR/$BOOKTITLE"
         mkdir -p "$OUTDIR"
+		printf "\nconverting m4b to mp3\n"
         MP3FILE="$OUTDIR/$BOOKTITLE.mp3"
         (${FFMPEG} -i "${BOOKLIST[(($INPUT-1))]}" -hide_banner -vn -acodec libmp3lame -ar 22050 -ab 64k "$MP3FILE")
         OUTDIR="$MP3DIR/$BOOKTITLE/Chapters"
@@ -69,24 +89,60 @@ convertToMp3(){
 }
 
 splitInChapters(){
-    (${FFMPEG} -i "$MP3FILE") 2> tmp.txt
-    CHAPTERCOUNT=`grep "Chapter #" tmp.txt | wc -l`
+	TMPFILE="$MP3DIR/$BOOKTITLE/tmp.txt"
+    (${FFMPEG} -i "$MP3FILE") 2> "$TMPFILE"
+	waitFile "$TMPFILE" 5 || {
+		echo "MP3 info file was not created after waiting for 5 seconds: '$TMPFILE'"
+		exit 1
+	}
+    CHAPTERCOUNT=`grep "Chapter #" "$TMPFILE" | wc -l`
     DIGITS=${#CHAPTERCOUNT}
     TRACK=1
     printf "Found %d chapters\n\n" $CHAPTERCOUNT
+    
+	# Get prefix zeros to pad chapter numbering in filenames
+	ZEROS=""
+	for (( c=1; c<$DIGITS; c++ ))
+    do
+        ZEROS="${ZEROS}0"
+    done
+
     while read -r first _ _ start _ end; do
         if [[ "${first}" = "Chapter" ]]
         then
             read  # discard line with Metadata:
             read _ _ chapter
             chapter=$(sed -re ":r;s/\b[0-9]{1,$((1))}\b/0&/g;tr" <<<$chapter)
-            chapter_file=`printf "%0${DIGITS}d - ${chapter}.mp3" $TRACK`
-            chapter_file=${chapter_file//[:]/꞉}  # replace colon with lookalike for Windows
-            printf "\nprocessing $TRACK/$CHAPTERCOUNT :: $chapter\n"
+			# printf "\n======\nextracted chaptername: %s\n" "${chapter}"
+
+            chapter_file=${chapter//[:]/꞉}  # replace colon with lookalike for Windows
+            chapter_file=${chapter_file//[*]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[\/]/_} # replace windows non allowed char to _
+            chapter_file=${chapter_file//[\\]/_} # replace windows non allowed char to _
+            chapter_file=${chapter_file//[?]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[\"]/_} # replace windows non allowed char to _
+            chapter_file=${chapter_file//[<]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[>]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[|]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[\^]/_}  # replace windows non allowed char to _
+            chapter_file=${chapter_file//[&]/_}  # replace windows non allowed char to _
+
+			# Get prefix zeros to pad chapter numbering in filenames
+			TRACKDIGITS=${#TRACK}
+			ZEROS=""
+			for (( c=1; c<=$((DIGITS-TRACKDIGITS)); c++ ))
+			do
+				ZEROS="${ZEROS}0"
+			done
+			chapter_file="${ZEROS}${TRACK} - ${chapter_file}.mp3"
+
+			printf "\nprocessing %d/%d :: %s\n" "$TRACK" "$CHAPTERCOUNT" "$chapter"
             </dev/null ${FFMPEG} -loglevel error -stats -i "${MP3FILE}" -ss "${start%?}" -to "${end}" -codec:a copy -metadata title="${chapter}" -metadata track="$TRACK/$CHAPTERCOUNT" "${OUTDIR}/${chapter_file}"
+             
             TRACK=$((TRACK+1))
         fi
-    done <tmp.txt
+    done <"$TMPFILE"
+	rm "$TMPFILE"
 }
 
 showBookList
@@ -100,9 +156,7 @@ convertToMp3
 
 splitInChapters
 
-rm tmp.txt
-
-echo "\n\nMP3 files have been saved to ${OUTDIR}"
+printf "\n\nMP3 files have been saved to: %s" "${OUTDIR}"
 
 printf "${NCLR}\n\n"
             
